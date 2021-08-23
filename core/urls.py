fa1acc9292e5d30c231001aaf5f87c5f924fdf8e
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-08-23 00:48:56 +0000

    views routing

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,13 @@
 from core.settings import MEDIA_ROOT
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('store.urls', namespace='store')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
