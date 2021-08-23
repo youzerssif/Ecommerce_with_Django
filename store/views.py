06ac7d6dde2e8a107347623151f6bddb3684d719
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2021-08-23 01:12:09 +0000

    views routing

--- store/views.py
+++ store/views.py
@@ -10,16 +10,16 @@ def categories(request):
 
 
 def all_products(request):
-    products = Product.products.all()
+    products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
 
 
-def category_list(request, category_slug=None):
-    category = get_object_or_404(Category, slug=category_slug)
-    products = Product.objects.filter(category=category)
-    return render(request, 'store/products/category.html', {'category': category, 'products': products})
+# def category_list(request, category_slug=None):
+#     category = get_object_or_404(Category, slug=category_slug)
+#     products = Product.objects.filter(category=category)
+#     return render(request, 'store/products/category.html', {'category': category, 'products': products})
 
 
-def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+# def product_detail(request, slug):
+#     product = get_object_or_404(Product, slug=slug, in_stock=True)
+#     return render(request, 'store/products/detail.html', {'product': product})
